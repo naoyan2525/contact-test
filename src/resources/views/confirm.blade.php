e3df0a3fafe63d51ddde7b829eac5e5ad9a71f90
naoya <n.hasepb0214@outlook.jp>
2026-04-07 00:53:59 +0900

    作業内容

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -3,62 +3,61 @@
 <head>
   <meta charset="UTF-8">
   <title>お問い合わせ確認</title>
+  <link rel="stylesheet" href="css/confirm.css">
 </head>
 <body>
 
 <div class="header">FashionablyLate</div>
-
 <div class="title">Confirm</div>
 
 <div class="confirm-form">
 
+
   <div class="row">
-    <div class="label">お名前</div>
-    <div class="value">山田 太郎</div>
+      <div class="label">お名前</div>
+      <div class="value" id="name"></div>
   </div>
 
   <div class="row">
     <div class="label">性別</div>
-    <div class="value">男性</div>
+    <div class="value" id="gender"></div>
   </div>
 
   <div class="row">
     <div class="label">メールアドレス</div>
-    <div class="value">test@example.com</div>
+    <div class="value" id="email"></div>
   </div>
 
   <div class="row">
     <div class="label">電話番号</div>
-    <div class="value">090-1234-5678</div>
+    <div class="value" id="tel"></div>
   </div>
 
   <div class="row">
     <div class="label">住所</div>
-    <div class="value">東京都〇〇区〇〇</div>
+    <div class="value" id="address"></div>
   </div>
 
   <div class="row">
     <div class="label">建物名</div>
-    <div class="value">〇〇マンション101</div>
+    <div class="value" id="building"></div>
   </div>
 
   <div class="row">
     <div class="label">お問い合わせの種類</div>
-    <div class="value">商品について</div>
+    <div class="value" id="category"></div>
   </div>
 
   <div class="row">
     <div class="label">お問い合わせ内容</div>
-    <div class="value">商品の詳細を教えてください。</div>
+    <div class="value" id="detail"></div>
   </div>
 
-  <!-- ボタン -->
   <div class="btn-area">
-    <button onclick="history.back()">修正する</button>
-    <button>送信する</button>
+    <button>送信</button>
+    <button onclick="history.back()">修正</button>
   </div>
 
 </div>
-
 </body>
 </html>
